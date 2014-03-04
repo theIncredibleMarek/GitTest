f4f4c2feec01d3071b38a227f294a06c5013f737
Marek FURAK <Marek FURAK@MaF-Lenovo>
2014-03-04 12:56:52 +0100

    Version 1.1

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("I am amazing");
     }
     
 }
